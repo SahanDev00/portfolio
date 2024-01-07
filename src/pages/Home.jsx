c3ce63e8e773c5b7a687f9ccce47c14c1f1440ec
SahanDev00 <dumidus86@gmail.com>
2024-01-07 22:45:47 +0530

    src/

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import Loader from '../components/Loader'
+
+const Home = () => {
+    return (
+        <section className='w-full h-screen relative'>
+            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                
+            </div>
+
+            <Canvas className='w-full h-screen relative' camera={{near: 0.1, far: 1000 }}>  
+            <Suspense fallback={<Loader/>}>
+
+            </Suspense>
+            </Canvas>
+            
+        </section>
+    )
+}
+
+export default Home
