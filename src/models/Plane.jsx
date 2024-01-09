a4db7cba2ab0653a53bad8fb35775fc3e6d5720c
SahanDev00 <dumidus86@gmail.com>
2024-01-09 21:49:28 +0530

    modified

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -2,12 +2,12 @@ import { useGLTF } from '@react-three/drei';
 import React from 'react'
 import planeScene from '../assets/3d/plane.glb';
 
-const Plane = () => {
+const Plane = ({isRotating, ...props}) => {
 
     const {scene, animations} = useGLTF(planeScene);
 
     return (
-        <mesh>
+        <mesh {...props}>
             <primitive object={scene}/>
         </mesh>
     )
